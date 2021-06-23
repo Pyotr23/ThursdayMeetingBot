ae46fbdd7236c0220d454b6d8ad2f2377b955d47
Pyotr <2chilavert3@mail.ru>
2021-06-23 09:37:17 +0300

    Начато написание сервисов для обработки сообщения от бота.

---
+++ src/TelegramBot/Interfaces/IBotMessageService.cs
@@ -0,0 +1,11 @@
+﻿namespace ThursdayMeetingBot.TelegramBot.Interfaces
+{
+    public interface IBotMessageService
+    {
+        /// <summary>
+        ///     Processing a message
+        /// </summary>
+        /// <param name="updateMessage">New message</param>
+        Task ProcessUpdateAsync(Update updateMessage);
+    }
+}
---
+++ src/TelegramBot/Services/BotService.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Options;
+using Telegram.Bot;
+using ThursdayMeetingBot.TelegramBot.Configurations;
+using ThursdayMeetingBot.TelegramBot.Interfaces;
+
+namespace ThursdayMeetingBot.TelegramBot.Services
+{
+    /// <inheritdoc />
+    public class BotService : IBotService
+    {
+        /// <inheritdoc />
+        public TelegramBotClient Client { get; }
+
+        /// <summary>
+        ///     Constructor
+        /// </summary>
+        /// <param name="botConfiguration"> Bot configuration. </param>
+        public BotService(IOptions<BotConfiguration> botConfiguration)
+        {
+            var accessToken = botConfiguration
+                .Value
+                .AccessToken;
+
+            Client = new TelegramBotClient(accessToken);
+        }
+    }
+}
