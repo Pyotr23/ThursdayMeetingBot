ec7e1557614473841cb649c37411d34af214b94f
Pyotr <2chilavert3@mail.ru>
2021-07-23 21:40:51 +0300

    Создана модель пользователя.

---
+++ src/libraries/Libraries.Data/Models/Base/AggregatedEntity.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ThursdayMeetingBot.Libraries.Data.Models.Base
+{
+    /// <summary>
+    ///     Base model for aggregated entities.
+    /// </summary>
+    /// <typeparam name="TKey"> Type of entity unique index. </typeparam>
+    public record AggregatedEntity<TKey> where TKey : IEquatable<TKey>
+    {
+        /// <summary>
+        ///     Unique identifier.
+        /// </summary>
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public TKey Id { get; set; }
+        
+        /// <summary>
+        ///     Created date.
+        /// </summary>
+        public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
+        
+        /// <summary>
+        ///     Updated date.
+        /// </summary>
+        public DateTime? UpdatedDate { get; set; } = DateTime.UtcNow;
+    }
+}
---
+++ src/libraries/Libraries.Data/Models/User.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ThursdayMeetingBot.Libraries.Data.Models.Base;
+
+namespace ThursdayMeetingBot.Libraries.Data.Models
+{
+    /// <summary>
+    ///     User.
+    /// </summary>
+    [Index(nameof(TelegramChatId))]
+    public record User : AggregatedEntity<long>
+    {
+        /// <summary>
+        ///     Unique identifier of user in Telegram.
+        /// </summary>
+        public int TelegramId { get; set; }
+        
+        /// <summary>
+        ///     User name that starts with "@".
+        /// </summary>
+        public string UserName { get; set; }
+        
+        /// <summary>
+        ///     User first name.
+        /// </summary>
+        /// 
+        public string FirstName { get; set; }
+        
+        /// <summary>
+        ///     User last name.
+        /// </summary>
+        public string LastName { get; set; }
+        
+        /// <summary>
+        ///     Unique identifier of telegram chat for current user.
+        /// </summary>
+        public long TelegramChatId { get; set; }
+    }
+}
