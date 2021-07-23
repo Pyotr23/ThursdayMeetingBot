e9c5e7bffdfec35042f0cf159a415aa196b2c0c9
Pyotr <2chilavert3@mail.ru>
2021-07-23 22:16:41 +0300

    Добавлен контекст БД.

---
+++ src/UnitTests/UnitTests.ThursdayMeetingBot.Libraries.Data/Extensions/StringExtensionsTests.cs
@@ -0,0 +1,78 @@
+﻿using ThursdayMeetingBot.Libraries.Data.Extensions;
+using Xunit;
+
+namespace UnitTests.ThursdayMeetingBot.Libraries.Data.Extensions
+{
+    public class StringExtensionsTests
+    {
+        [Fact]
+        public void ToSnakeCase_NullString_ReturnNull()
+        {
+            string inputString = null;
+
+            var outputString = inputString.ToSnakeCase();
+
+            Assert.Null(outputString);
+        }
+
+        [Fact]
+        public void ToSnakeCase_EmptyString_ReturnEmptyString()
+        {
+            var inputString = string.Empty;
+
+            var outputString = inputString.ToSnakeCase();
+
+            Assert.Equal(string.Empty, outputString);
+        }
+
+        [Fact]
+        public void ToSnakeCase_CamelCaseString_ReturnSnakeCaseString()
+        {
+            const string inputString = "OneTwoThree";
+
+            var outputString = inputString.ToSnakeCase();
+
+            Assert.Equal("one_two_three", outputString);
+        }
+
+        [Fact]
+        public void ToSnakeCase_SmallCamelCaseString_ReturnSnakeCaseString()
+        {
+            const string inputString = "oneTwoThree";
+
+            var outputString = inputString.ToSnakeCase();
+
+            Assert.Equal("one_two_three", outputString);
+        }
+
+        [Fact]
+        public void ToSnakeCase_SnakeCaseString_ReturnSnakeCaseString()
+        {
+            const string inputString = "one_two_three";
+
+            var outputString = inputString.ToSnakeCase();
+
+            Assert.Equal(outputString, outputString);
+        }
+
+        [Fact]
+        public void ToSnakeCase_SnakeCaseStringWithDigitsBeforeUpperCase_ReturnSnakeCaseString()
+        {
+            const string inputString = "One1Two2Three3";
+
+            var outputString = inputString.ToSnakeCase();
+
+            Assert.Equal("one1_two2_three3", outputString);
+        }
+
+        [Fact]
+        public void ToSnakeCase_SnakeCaseStringWithDigitsInWordMiddle_ReturnSnakeCaseString()
+        {
+            const string inputString = "On1eTw2oThre3e";
+
+            var outputString = inputString.ToSnakeCase();
+
+            Assert.Equal("on1e_tw2o_thre3e", outputString);
+        }
+    }
+}
---
+++ src/libraries/Libraries.Data/Contexts/BotDbContext.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ThursdayMeetingBot.Libraries.Data.Extensions;
+using ThursdayMeetingBot.Libraries.Data.Models;
+
+namespace ThursdayMeetingBot.Libraries.Data.Contexts
+{
+    /// <summary>
+    ///     Db context for connecting bot data.
+    /// </summary>
+    public class BotDbContext : DbContext
+    {
+        /// <summary>
+        ///     Constructor.
+        /// </summary>
+        /// <param name="options">Options for creating bot context. </param>
+        public BotDbContext(DbContextOptions<BotDbContext> options)
+            : base(options) {}
+        
+        /// <summary>
+        ///     Table with users.
+        /// </summary>
+        public DbSet<User> Users { get; set; }
+        
+        /// <summary>
+        ///     Method executing while models creating.
+        /// </summary>
+        /// <param name="modelBuilder"> Model builder. </param>
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            foreach (var entity in modelBuilder.Model.GetEntityTypes())
+            {
+
+                // Replace table names
+                entity.SetTableName(entity.GetTableName().ToSnakeCase());
+
+                // Replace column names
+                foreach (var property in entity.GetProperties())
+                {
+                    property.SetColumnName(property.Name.ToSnakeCase());
+                }
+
+                foreach (var key in entity.GetKeys())
+                {
+                    key.SetName(key.GetName().ToSnakeCase());
+                }
+
+                foreach (var key in entity.GetForeignKeys())
+                {
+                    key.PrincipalKey.SetName(key.PrincipalKey.GetName().ToSnakeCase());
+                }
+
+                foreach (var index in entity.GetIndexes())
+                {
+                    index.SetDatabaseName(index.GetDatabaseName().ToSnakeCase());
+                }
+            }
+        }
+    }
+}
