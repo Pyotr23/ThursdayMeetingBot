722f19852335168dfbd711e4254c901136540cce
Pyotr <2chilavert3@mail.ru>
2021-08-15 01:34:41 +0300

    Удалён неиспользуемый код.

--- src/ThursdayMeetingBot.Web/Models/Notification/NotificationDateTime.cs
+++ src/ThursdayMeetingBot.Web/Models/Notification/NotificationDateTime.cs
@@ -8,18 +8,13 @@ namespace ThursdayMeetingBot.Web.Models.Notification
     /// <summary>
     ///     Notification time class.
     /// </summary>
-    internal record NotificationDateTime
+    internal sealed record NotificationDateTime
     {
         /// <summary>
         ///     Value.
         /// </summary>
         private DateTime Value { get; }
-        
-        /// <summary>
-        ///     Day of week.
-        /// </summary>
-        private DayOfWeek DayOfWeek => Value.DayOfWeek;
-        
+
         /// <summary>
         ///     Russian description of day of week.
         /// </summary>
@@ -30,40 +25,7 @@ namespace ThursdayMeetingBot.Web.Models.Notification
         /// </summary>
         internal string MoscowShortTime => Value.ToMoscowTime().ToShortTimeString();
         
-        /// <summary>
-        ///     Time until notification.
-        /// </summary>
-        internal TimeSpan DueTime => Value - DateTime.UtcNow;
-
-        /// <summary>
-        ///     Formatted time until notification.
-        /// </summary>
-        private string FormattedDueTime => DueTime.ToString(@"d\.hh\:mm");
-
-        // /// <summary>
-        // ///     Bot answer.
-        // /// </summary>
-        // public string BotMessage => 
-        //     string.Format(BotAnswer.NotificationsAreEnabled,
-        //         RussianDayOfWeekName,
-        //         MoscowShortTime);
-        
-        /// <summary>
-        ///     Message for logging.
-        /// </summary>
-        public string LogMessage =>
-            $"Notifications are enabled, every {DayOfWeek}, {MoscowShortTime}, after {FormattedDueTime}.";
-
-        /// <summary>
-        ///     Constructor.
-        /// </summary>
-        /// <param name="dateTime"> The date and time to create the entity for. </param>
-        public NotificationDateTime(DateTime dateTime)
-        {
-            Value = dateTime;
-        }
-
-        public NotificationDateTime(NotificationConfiguration configuration)
+        internal NotificationDateTime(NotificationConfiguration configuration)
         {
             Value = DateTimeHelper.GetCurrentWeekNotificationDateTime(configuration);
         }
